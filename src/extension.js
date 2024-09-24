3306003f9513252f78fc4db3a1ccbde4f91a5655
Dag Andreas <daanfofo@gmail.com>
2024-09-24 08:57:01 +0200

    completed core and extension

--- src/extension.js
+++ src/extension.js
@@ -17,12 +17,13 @@ const book = {
 
 /* eslint-disable no-unused-vars */
 const isbn13 = '978-0132350884'
+book.isbn.isbn13 = isbn13
 
 // 1. Set this to the book name - using the book object
 const name = book.name
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = book.isbn10
+const isbn10 = book.isbn.isbn10
 
 // Do not modify this basket object directly
 const basket = {
@@ -45,7 +46,18 @@ const basket = {
 const numberOfVoucherCodes = basket.voucherCodes.length
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = basket.items[0]
+const firstVoucherCode = basket.voucherCodes[0]
+
+
+book.category = 'Programming'
+
+delete book.isbn.asin
+book.pages = 464
+
+delete book.dimensions
+
+basket.items[0].price = 2
+basket.items.push({name: 'Oranges', price: 0.75, quantity: 4})
 
 // Do not edit this exported object
 module.exports = {
@@ -54,5 +66,5 @@ module.exports = {
   book,
   basket,
   numberOfVoucherCodes,
-  firstVoucherCode
+  firstVoucherCode,
 }
